368153d620103a4c4f506e7fab7652e80d6853b0
thisischaga <redocstar@gmail.com>
2026-02-01 12:23:11 +0100

    deploiement

--- Congfig/db.js
+++ Congfig/db.js
@@ -7,12 +7,9 @@ dotenv.config();
 const connectDB = ()=>{
 
     const mongoURI = process.env.MONGO_URI;
-    mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("✅ Connexion à MongoDB réussie !"))
-.catch(err => console.error("❌Erreur de connexion à MongoDB :", err));
+    mongoose.connect(mongoURI)
+    .then(() => console.log("✅ Connexion à MongoDB réussie !"))
+    .catch(err => console.error("❌Erreur de connexion à MongoDB :", err));
 }
 
 module.exports = connectDB;
